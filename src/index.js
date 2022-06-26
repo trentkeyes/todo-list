ceb30c65a72319154cdf3f77a87b50383546f133
trentkeyes <tkeyes@gmail.com>
2022-06-25 20:29:52 -0500

    Move list items above add form, style add task button

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import { todoData } from "./todoData";
 import { format, compareAsc } from "date-fns";
 
-// format(new Date(2014, 1, 11), "MM/dd/yyyy");
-// //=> '02/11/2014'
-
-// const dates = [
-//   new Date(1995, 6, 2),
-//   new Date(1987, 1, 11),
-//   new Date(1989, 6, 10),
-// ];
-// console.log(dates.sort(compareAsc));
-
-// const myDate = format(new Date(2022, 10, 10), "PPPP");
-
-// console.log(myDate);
-
 //dom stuff
 
 const inputTodo = (e) => {
@@ -32,14 +18,15 @@ const displayTodo = () => {
   const list = document.querySelector(".todo-list");
   const listItem = document.createElement("li");
   listItem.classList.add("todoItem");
-  list.appendChild(listItem);
+  list.prepend(listItem);
   const newTodo = document.createElement("label");
   newTodo.classList.add("todoText");
   newTodo.textContent = todoData.getNewTodo();
   const hr = document.createElement("hr");
+  // hr.setAttribute("width", "100px");
   listItem.appendChild(checkbox);
   listItem.appendChild(newTodo);
-  list.appendChild(hr);
+  listItem.after(hr);
 };
 
 const addTaskButton = document.querySelector("#taskButton");

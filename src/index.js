a04c7735b0ab6d8218583afcf1d7e59701afcd66
trentkeyes <tkeyes@gmail.com>
2022-07-04 14:38:21 -0500

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+import { events } from '/src/dom/events';
+
 // figure out how to save data
 
 // fix styling on modified todos
 
 // create a flex box for date and priority
+
+// new js application
+
+// priority isn't getting added
+
+// add github logo
+
+//rename repo / project with lower case
+
+// coulc reate a parameter to configure application
+
+//current project id param
